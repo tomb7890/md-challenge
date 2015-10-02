9cdc5a8a97b4763b29509be0f8127129e841d57c
Tom Billingsley <tomb@antec.local>
2015-10-02 15:56:12 -0400

    tests for non-positive integers

--- fibonacci/spec/fibonacci_spec.rb
+++ fibonacci/spec/fibonacci_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../fibonacci"
 
-#          3  4  5  6
+#    1  2  3  4  5  6
    # 1  1  2  3  5  8
 
 RSpec.describe "Fibonacci" do
@@ -10,13 +10,29 @@ RSpec.describe "Fibonacci" do
       expect(f).to eq(1)
     end
 
-    it "test two"  do
+    it "test three"  do
       f = fib_number(3)
       expect(f).to eq(2)
     end
 
-    it "test three"  do
+    it "test six"  do
       f = fib_number(6)
       expect(f).to eq(8)
     end
+
+    it "test negative one"  do
+      f = fib_number(-1)
+      expect(f).to eq(nil)
+    end
+
+    it "test large negative numbers"  do
+      f = fib_number(-99999)
+      expect(f).to eq(nil)
+    end
+
+    it "test zero"  do
+      f = fib_number(0)
+      expect(f).to eq(nil)
+    end
+
 end
