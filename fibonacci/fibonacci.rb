ecea229840862110b5bcb5b24ff7ba58b4e4b7b8
Tom Billingsley <tomb@antec.local>
2015-10-02 14:56:25 -0400

    some minor tidying up

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,25 +1,14 @@
 
-# 6:34am completed initial revision of fibonacii code
-
 def fib_number(n)
-
-#          3  4  5  6
-   # 1  1  2  3  5  8
-
-  sum = 1
-  # need a data structure to record the history
   a = Array.new
-
   a[0] = 0
   a[1] = 1
 
   x = 1
   (1...n).each do
     x = a[2] = a[0] + a[1]
-    puts "x = #{x}"
     a[0] = a[1]
     a[1] = a[2]
   end
-
   x
 end
