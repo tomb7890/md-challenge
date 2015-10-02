ece31bf381a0da91a9db3370a07af9e396ae1d93
Tom Billingsley <tomb@antec.local>
2015-10-02 15:54:42 -0400

    return nil for negative numbers and zero.

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,10 +1,18 @@
 
 def fib_number(n)
+  # Compute the Fibonacci number for an index n given a sequence that
+  # starts with 1.  Returns nil for negative numbers.
+  #
+
   a = Array.new
   a[0] = 0
   a[1] = 1
 
-  x = 1
+  x = nil
+  if n > 0
+    x = 1
+  end
+
   (1...n).each do
     x = a[2] = a[0] + a[1]
     a[0] = a[1]
