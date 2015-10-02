62fd772dcc0590bc794ace00fe23fde72a961aa7
Tom Billingsley <tomb@antec.local>
2015-10-02 14:58:55 -0400

    minor tiyding up

--- fibonacci/spec/fibonacci_spec.rb
+++ fibonacci/spec/fibonacci_spec.rb
@@ -1,20 +1,22 @@
 require_relative "../fibonacci"
 
+#          3  4  5  6
+   # 1  1  2  3  5  8
+
 RSpec.describe "Fibonacci" do
 
     it "test one"  do
-      actual = fib_number(1)
-      expect(actual).to eq(1)
+      f = fib_number(1)
+      expect(f).to eq(1)
     end
 
     it "test two"  do
-      actual = fib_number(3)
-      expect(actual).to eq(2)
+      f = fib_number(3)
+      expect(f).to eq(2)
     end
 
     it "test three"  do
-      actual = fib_number(6)
-      expect(actual).to eq(8)
+      f = fib_number(6)
+      expect(f).to eq(8)
     end
-
 end
